55c50b17e4493988759ab72c64c5c1d40ef48aa4
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2018-05-05 12:49:45 -0300

    feat: Add temperature tracker object
    TemperatureTracker is an object that behaves like a list.
    We can insert temperatures and get the min, max and mean temperature
    values.

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# temperature_tracker
+# Temperature Tracker
+
 Simple temperature tracker
+
+Requirements:
+
+* Python 3.6+
+
+## What is this?
+
+This is a simple temperature tracker object. The structure is composed by the following API:
+
+`insert`: records a new temperature.
+
+`get_max`: returns the highest temperature we have seen so far.
+
+`get_min`: returns the lowest temperature we have seen so far.
+
+`get_mean`: returns the mean of all temperatures we have seen so far.
+
+## Examples
+
+To use the TemperatureTracker, you need to spawn a new Python shell or write
+the following lines into a file:
+
+```python
+from tracker import TemperatureTracker
+
+temperature_tracker = TemperatureTracker()
+
+# Registering temperatures
+temperature_tracker.insert(22)
+temperature_tracker.insert(23)
+temperature_tracker.insert(21)
+
+# Checking what is the maximum temperature we have collected so far
+temperature_tracker.get_max()
+
+# Checking what is the minimum temperature we have collected so far
+temperature_tracker.get_min()
+
+# Checking what is the mean temperature we have collected so far
+temperature_tracker.get_mean()
+```
+
+
+You can read the code and tests to better understand how it works.
---
+++ tracker.py
@@ -0,0 +1,100 @@
+import unittest
+
+
+class InvalidTemperature(Exception):
+    """Raise when an invalid temperature is
+    added to the tracker."""
+    pass
+
+
+class TemperatureTracker(object):
+
+    def __init__(self):
+        self.temperatures = []
+
+    def insert(self, temperature: int) -> None:
+        """Insert a new temperature on the tracker.
+        It is added in the end the collection, keeping
+        the order from the most recent to the oldest temperature."""
+        # Check temperature value
+        self.validate_temperature(temperature)
+        # Record the temperature
+        self.temperatures.append(temperature)
+
+    def validate_temperature(self, temperature: int) -> None:
+        """Check if the temperature is a valid value."""
+        if (temperature < 0 or temperature > 100) or not isinstance(temperature, int):
+            raise InvalidTemperature(
+                'Temperature must be an integer between 0 and 100')
+
+    def get_max(self):
+        """Return the maximum temperature tracked."""
+        return max(self.temperatures)
+
+    def get_min(self):
+        """Return the minimum temperature tracked."""
+        return min(self.temperatures)
+
+    def get_mean(self) -> float:
+        """Return the mean temperature tracked."""
+        return sum(self.temperatures) / len(self.temperatures)
+
+    def __getitem__(self, index: int) -> int:
+        """Return the element in the given index.
+        This function overrides the list indexing protocol.
+        Example:
+            >>> temperature_tracker = TemperatureTracker()
+            >>> temperature_tracker.insert(15)
+            >>> temperature_tracker[0]
+            >>> 15
+        """
+        return self.temperatures[index]
+
+
+class TestTemperatureTracker(unittest.TestCase):
+
+    def test_insert(self):
+        temperature_tracker = TemperatureTracker()
+        temperature_tracker.insert(22)
+        self.assertEqual(temperature_tracker[-1], 22)
+        temperature_tracker.insert(21)
+        self.assertEqual(temperature_tracker[-1], 21)
+
+    def test_insert_with_invalid_temperature(self):
+        temperature_tracker = TemperatureTracker()
+        # Insert a value that is too low...
+        with self.assertRaises(InvalidTemperature):
+            temperature_tracker.insert(-3)
+
+        # Insert a value that is too high...
+        with self.assertRaises(InvalidTemperature):
+            temperature_tracker.insert(101)
+
+        # Insert a non-integer value...
+        with self.assertRaises(InvalidTemperature):
+            temperature_tracker.insert(3.0)
+
+    def test_get_max(self):
+        temperature_tracker = TemperatureTracker()
+        temperature_tracker.insert(10)
+        temperature_tracker.insert(15)
+        temperature_tracker.insert(12)
+        self.assertEqual(temperature_tracker.get_max(), 15)
+
+    def test_get_min(self):
+        temperature_tracker = TemperatureTracker()
+        temperature_tracker.insert(12)
+        temperature_tracker.insert(8)
+        temperature_tracker.insert(10)
+        self.assertEqual(temperature_tracker.get_min(), 8)
+
+    def test_get_mean(self):
+        temperature_tracker = TemperatureTracker()
+        temperature_tracker.insert(3)
+        temperature_tracker.insert(4)
+        temperature_tracker.insert(5)
+        self.assertEqual(temperature_tracker.get_mean(), 4.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
